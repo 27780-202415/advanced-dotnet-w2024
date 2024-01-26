48362402ea09bf536f5ca47de0544978c0fd5210
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2024-01-25 23:12:07 -0500

    Week03-P2

---
+++ Week03_2_SqlQueryBuilder/Circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week03_2_SqlQueryBuilder
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+    }
+}
---
+++ Week03_2_SqlQueryBuilder/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week03_2_SqlQueryBuilder
+{
+    public class Owner : IDatabaseTable
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+
+    }
+}
